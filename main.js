ddd9b67254798578497f4b235e55ae228ad4c8a0
Brian Pineau <pinoub@gmail.com>
2019-08-25 12:23:41 -0500

    2nd header drop down created.

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
 
-const changeBackground = () => {
-    document.getElementById('bannerBox')
+
+document.getElementById("header-snowboard-button").addEventListener("click", openMenu);
+
+
+function openMenu() {
+    document.getElementById("snowboard-drop").classList.toggle("active");
 }
 
-changeBackground();
+
+document.getElementById("header-ski-button").addEventListener("click", openMenu2);
+
+
+function openMenu2() {
+    document.getElementById("ski-drop").classList.toggle("active");
+}
