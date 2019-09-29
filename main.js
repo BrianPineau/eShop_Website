d946ad321486c8e57843a411e831f1fccf224a1f
Brian Pineau <pinoub@gmail.com>
2019-09-28 20:41:50 -0500

    side menu height adjusts to header height.

--- main.js
+++ main.js
@@ -7,7 +7,6 @@ const addCart = document.querySelector('.button-1');
 const cartUpdate = document.querySelector('.header-cart-item');
 
 /*  Header Scroll Pop Out  */
-
 window.addEventListener('scroll', function() {
     if (window.pageYOffset > 40) {
         headerCollapse.classList += ' pop-out';
@@ -25,7 +24,7 @@ window.addEventListener('scroll', function() {
 function openMenu() {
     document.getElementById("snowboard-drop").classList.toggle("active");
 }
-
+//Click outside of drop menu to close menu
 window.addEventListener('mouseup', function (event) {
     const snowboardAway = document.getElementById("snowboard-drop");
     if (event.target != snowboardAway) {
@@ -37,29 +36,33 @@ function openMenu2() {
     document.getElementById("ski-drop").classList.toggle("active");
 }
 
-/*  Mobile Drop Down Header  */
+/*  Search Bar Appear  */
+function searchAppear() {
+    document.querySelector("search-text img").classList.toggle("search-text-appear");
+}
+
+
+/*  Mobile Side Menu  */
 let toggleNavStatus = false;
 
-let toggleNav = function (){
+let toggleNav = function (event){
     let mobileDrop = document.querySelector(".mobile-header-drop"); 
+    let mobileDropOffset = headerCollapse.offsetHeight;
     let moveBody = document.querySelector("body");
+
+//Set height of side menu
+    mobileDrop.style.top = mobileDropOffset + 'px';
     
-  //drop header extends with button click
+//menu extends with button click
     if (toggleNavStatus === false) {
         mobileDrop.style.left = "0";
-        moveBody.style.position = "fixed";
-        moveBody.style.top = "0";
-        moveBody.style.left = "300px";
         toggleNavStatus = true;
+
+//menu retracts
     } else if (toggleNavStatus === true) {
         mobileDrop.style.left = "-300px";
-        moveBody.style.top = "0";
-        moveBody.style.left = "0";
-        moveBody.style.position = "static";
         toggleNavStatus = false;
     }
-    
-
 
 }
 
