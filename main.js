9c12fc11c2dbcba5608302885cd66a26c66df9c0
Brian Pineau <pinoub@gmail.com>
2019-11-07 16:30:28 -0600

    updated header links on 3 html docs.

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 
 const headerCollapse = document.querySelector('header');
 const moveIcons = document.querySelector('.icon-set');
-const snowboardBtn = document.getElementById("header-snowboard-button");
-const skiBtn = document.getElementById("header-ski-button");
 const openButton = document.querySelector(".toggler");
 
 /*  Header Scroll Pop Out  */
@@ -15,25 +13,6 @@ window.addEventListener('scroll', function() {
         moveIcons.classList = ' icon-set';
     }
 });
-
-
-/* Header Drop Menus */
-let toggleMenu = false;
-
-let openMenu = function () {
-let dropMenu = document.getElementById("snowboard-drop");
-    //Menu appears
-    if (toggleMenu === false) {
-        dropMenu.classList = "block";
-        toggleMenu = true;
-    
-    //Menu disappears on button click or click outside
-    } else if (toggleMenu === true) {
-        dropMenu.style.display = "none";
-        toggleMenu = false;
-  }
-
-}
 
 /*  Search Bar Appear  */
 let toggleSearch = false;
@@ -75,15 +54,7 @@ let toggleNav = function () {
 }
 
 
-/*  Cart Full Indicator  */
-document.querySelector("header-cart-dot").style.border = "block";
-document.querySelector("header").style.backgroundColor = "green";
-
-document.queryselector("header-cart-dot").style.display = "block";
-
 /*  Event Listeners  */
 searchToggle.addEventListener("click", navAppear);
-snowboardBtn.addEventListener("click", openMenu);
 skiBtn.addEventListener("click", openMenu2);
 openButton.addEventListener("click", toggleNav);
-dotButton.addEventListener("click", cartFull);
