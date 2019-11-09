670f109a66af42a5c8a7e27ac768c09c3485995f
Brian Pineau <pinoub@gmail.com>
2019-11-09 17:03:08 -0600

    Fixed alignment issue between hero image and header on iteminfopg.

--- Resources/JS-Folder/item-info-pg.js
+++ Resources/JS-Folder/item-info-pg.js
@@ -7,3 +7,65 @@ function dotAppear() {
 }
 
 dotButton.addEventListener("click", dotAppear);
+
+
+
+const headerCollapse = document.querySelector('header');
+const moveIcons = document.querySelector('.icon-set');
+const openButton = document.querySelector(".toggler");
+
+/*  Header Scroll Pop Out  */
+window.addEventListener('scroll', function() {
+    if (window.pageYOffset > 40) {
+        headerCollapse.classList += ' pop-out';
+        moveIcons.classList += ' icon-move';
+    } else {
+        headerCollapse.classList = 'header';
+        moveIcons.classList = ' icon-set';
+    }
+});
+
+/*  Search Bar Appear  */
+let toggleSearch = false;
+let searchToggle = document.querySelector('.search-catalyst');
+let searchAppear = document.querySelector('.search-text');
+
+let navAppear = () => {
+    if (toggleSearch === false) {
+        searchAppear.style.display = 'block';
+        searchAppear.style.width = '150px';
+        toggleSearch = true;
+    } else if (toggleSearch === true) {
+        searchAppear.style.display = 'none';
+        searchAppear.style.width = 0;
+        toggleSearch = false;
+    }
+}
+
+
+
+/*  Mobile Side Menu  */
+
+let toggleNavStatus = false;
+
+let toggleNav = function () {
+    const mobileDrop = document.querySelector(".mobile-header-drop");
+    // const menuWrap = document.querySelector(".menu-wrap");
+    
+//menu extends with button click
+    if (toggleNavStatus === false) {
+        mobileDrop.style.left = "-38px";
+        toggleNavStatus = true;
+
+//menu retracts
+    } else if (toggleNavStatus === true) {
+        mobileDrop.style.left = "-338px";
+        toggleNavStatus = false;
+    }
+}
+
+
+/*  Event Listeners  */
+searchToggle.addEventListener("click", navAppear);
+skiBtn.addEventListener("click", openMenu2);
+openButton.addEventListener("click", toggleNav);
