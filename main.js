eb84d973f7e211a5d08d74bd43d325620e578bb5
Brian Pineau <brianpineau@Brians-iMac.local>
2020-03-11 22:12:43 -0500

    Created slide animation for banner in css

--- main.js
+++ main.js
@@ -60,105 +60,11 @@ const imageTwo = document.getElementById('image-2');
 const leftArrow = document.getElementById('arrow-left');
 const rightArrow = document.getElementById('arrow-right');
 const imageNumberDisp = document.getElementById('banner-image-counter');
-let heroCounter = 1;
 
-window.onload = () => {
-    nextHeroLoop();
-}
 
-const movePicLeft = () => {
-    if(heroCounter === 1) {
-        imageOne.style.opacity = '0';
-        
-        imageZero.style.right = '0px';
-        imageZero.style.zIndex = '1500';
-        imageOne.style.right = '-100%';
-        imageOne.style.zIndex = '500';
-        imageTwo.style.right = '100%';
-        imageTwo.style.zIndex = '1000';
-
-        imageOne.style.opacity = '1';
-        setTimeout(function () {
-            heroCounter = 3;
-        }, 500);
-    } else if (heroCounter === 3) {
-        imageTwo.style.opacity = '0';
-
-        imageOne.style.right = '0px';
-        imageOne.style.zIndex = '1500';
-        imageTwo.style.right = '100%';
-        imageTwo.style.zIndex = '500';
-        imageZero.style.right = '-100%';
-        imageZero.style.zIndex = '1000';
-
-        imageTwo.style.opacity = '1';
-        setTimeout(function () {
-            heroCounter = 2;
-        }, 500);
-    } else if (heroCounter === 2) {
-        imageZero.style.opacity = '0';
-
-        imageTwo.style.right = '0px';
-        imageTwo.style.zIndex = '1500';
-        imageZero.style.right = '100%';
-        imageZero.style.zIndex = '500';
-        imageOne.style.right = '-100%';
-        imageOne.style.zIndex = '1000';
-        
-        imageZero.style.opacity = '1';
-        setTimeout(function () {
-            heroCounter = 1;
-        }, 500);
-    };
-    imageNumberDisp.innerHTML = `${heroCounter}/3`;
-}
-
-const nextHeroLoop = () => {
-    if(heroCounter === 1) {
-        imageOne.style.opacity = '0';
-        
-        imageZero.style.right = '0px';
-        imageZero.style.zIndex = '1500';
-        imageOne.style.right = '-100%';
-        imageOne.style.zIndex = '500';
-        imageTwo.style.right = '100%';
-        imageTwo.style.zIndex = '1000';
-        imageOne.style.opacity = '1';
-        setTimeout(function () {
-            heroCounter = 2;
-        }, 500);
-    } else if (heroCounter === 2) {
-        imageTwo.style.opacity = '0';
-
-        imageOne.style.right = '0px';
-        imageOne.style.zIndex = '1500';
-        imageTwo.style.right = '-100%';
-        imageTwo.style.zIndex = '500';
-        imageZero.style.right = '100%';
-        imageZero.style.zIndex = '1000';
-        imageTwo.style.opacity = '1';
-        setTimeout(function () {
-            heroCounter = 3;
-        }, 500);
-    } else if (heroCounter === 3) {
-        imageZero.style.opacity = '0';
-
-        imageTwo.style.right = '0px';
-        imageTwo.style.zIndex = '1500';
-        imageZero.style.right = '-100%';
-        imageZero.style.zIndex = '500';
-        imageOne.style.right = '100%';
-        imageOne.style.zIndex = '1000';
-        imageZero.style.opacity = '1';
-        setTimeout(function () {
-            heroCounter = 1;
-        }, 500);
-    };
-    imageNumberDisp.innerHTML = `${heroCounter}/3`;
-}
 
 leftArrow.addEventListener('click', movePicLeft);
-rightArrow.addEventListener('click', nextHeroLoop);
+rightArrow.addEventListener('click', nextImage);
 
 
 
@@ -166,3 +72,118 @@ rightArrow.addEventListener('click', nextHeroLoop);
 searchToggle.addEventListener("click", navAppear);
 skiBtn.addEventListener("click", openMenu2);
 openButton.addEventListener("click", toggleNav);
+
+
+
+
+
+
+// /*   Banner Image Change   */
+// const imageZero = document.getElementById('image-0');
+// const imageOne = document.getElementById('image-1');
+// const imageTwo = document.getElementById('image-2');
+// const leftArrow = document.getElementById('arrow-left');
+// const rightArrow = document.getElementById('arrow-right');
+// const imageNumberDisp = document.getElementById('banner-image-counter');
+// let heroCounter = 1;
+
+// window.onload = () => {
+//     nextImage();
+//     movePicLeft();
+// }
+
+
+// //Change banner image to next one on the LEFT
+// const movePicLeft = () => {
+//     if(heroCounter === 1) {
+//         imageOne.style.opacity = '0';
+//         imageZero.style.left = '0px';
+//         imageZero.style.zIndex = '1500';
+//         imageOne.style.left = '100%';
+//         imageOne.style.zIndex = '1000';
+//         imageTwo.style.left = '-100%';
+//         imageTwo.style.zIndex = '500';
+//         imageOne.style.opacity = '1';
+//             heroCounter = 3;
+//             return;
+//     } else if (heroCounter === 3) {
+//         imageTwo.style.opacity = '0';
+//         imageOne.style.left = '0px';
+//         imageOne.style.zIndex = '1500';
+//         imageTwo.style.left = '100%';
+//         imageTwo.style.zIndex = '1000';
+//         imageZero.style.left = '-100%';
+//         imageZero.style.zIndex = '500';
+//         imageTwo.style.opacity = '1';
+//             heroCounter = 2;
+//             return;
+//     } else if (heroCounter === 2) {
+//         imageZero.style.opacity = '0';
+//         imageTwo.style.left = '0px';
+//         imageTwo.style.zIndex = '1500';
+//         imageZero.style.left = '100%';
+//         imageZero.style.zIndex = '1000';
+//         imageOne.style.left = '-100%';
+//         imageOne.style.zIndex = '500';
+//         imageZero.style.opacity = '1';
+//             heroCounter = 1;
+//             return;
+//     };
+//     imageNumberDisp.innerHTML = `${heroCounter}/3`;
+//     return {
+//         getHeroCounter: function () {
+//             return heroCounter;
+//         }
+//     }
+// }
+
+// //Change banner image to next one on the RIGHT
+// const nextImage = () => {
+//     if(heroCounter === 1) {
+//         imageOne.style.opacity = '0';
+//         imageZero.style.right = '0px';
+//         imageZero.style.zIndex = '1500';
+//         imageOne.style.right = '-100%';
+//         imageOne.style.zIndex = '500';
+//         imageTwo.style.right = '100%';
+//         imageTwo.style.zIndex = '1000';
+//         imageOne.style.opacity = '1';
+//         heroCounter = 2;
+//         imageNumberDisp.innerHTML = `${heroCounter}/3`;
+//         return;
+//     } else if (heroCounter === 2) {
+//         imageTwo.style.opacity = '0';
+//         imageOne.style.right = '0px';
+//         imageOne.style.zIndex = '1500';
+//         imageTwo.style.right = '-100%';
+//         imageTwo.style.zIndex = '500';
+//         imageZero.style.right = '100%';
+//         imageZero.style.zIndex = '1000';
+//         imageTwo.style.opacity = '1';
+//         heroCounter = 3;
+//         imageNumberDisp.innerHTML = `${heroCounter}/3`;
+//         return;
+//     } else if (heroCounter === 3) {
+//         imageZero.style.opacity = '0';
+//         imageTwo.style.right = '0px';
+//         imageTwo.style.zIndex = '1500';
+//         imageZero.style.right = '-100%';
+//         imageZero.style.zIndex = '500';
+//         imageOne.style.right = '100%';
+//         imageOne.style.zIndex = '1000';
+//         imageZero.style.opacity = '1';
+//         heroCounter = 1;
+//         imageNumberDisp.innerHTML = `${heroCounter}/3`;
+//         return;
+//     };
+//     return {
+//         getHeroCounter: function () {
+//             return heroCounter;
+//         }
+//     }
+// }
+
+// console.log(heroCounter);
+
+// leftArrow.addEventListener('click', movePicLeft);
+// rightArrow.addEventListener('click', nextImage);
